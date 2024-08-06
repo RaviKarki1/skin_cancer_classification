d780d3dc7401d143421548ef731cbdf5e85433a2
RAVI KARKI <ravi@192-168-1-6.tpgi.com.au>
2024-08-06 13:05:55 +1000

    Added react frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import SkinCancerClassifier from './Pages/Classifier'
+
+function App() {
+  return (
+    <div className="App">
+      <SkinCancerClassifier />
+    </div>
+  );
+}
+
+export default App;
