3d4d6488f998921eb9e1bdac9a7b3d766dca01a2
RAVI KARKI <ravi@192-168-1-10.tpgi.com.au>
2024-08-27 23:40:12 +1000

    updated the frontend behaviour. Connected fronted with flask middleware. updated flask to reply to frontend

--- backend/flask_classifier.py
+++ backend/flask_classifier.py
@@ -1,9 +1,26 @@
-from flask import Flask, render_template, request
-import jsonify
-import requests
-import pickle
-import numpy as np
-import sklearn
-from sklearn.preprocessing import StandardScaler
+import time
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, origins=["http://localhost:3000", "http://localhost:3000"])
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    print("photo upload initialised")
+    if 'file' not in request.files:
+        return jsonify({"error":"No file found"}), 400
+    
+    file = request.files['file']
+    
+    if file.filename == '':
+        return jsonify({"error" : "No selected file"}), 400
+    
+    if file:
+        return jsonify({
+            "message":"flask prediction"
+        })
+        
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,33 @@
-import React, {useState} from "react"
+import React, {useState, useEffect} from "react"
 import './App.css';
 import SkinCancerClassifier from "./Pages/Classifier"
 import PhotoUpload from './Components/PhotoUpload';
 
 function App() {
+  const [photo, setPhoto] = useState();
   const [photoUploaded, setPhotoUploaded] = useState();
   const [predicted, setPredicted] = useState();
+  //api test
+  // const [time, setTime] = useState();
+  
+  // useEffect(() => {
+  //   fetch('/time')
+  //               .then(response => response.json())
+  //               .then(data => {
+  //                 console.log(data)
+  //                 setTime(data.time)
+  //               })
+  //               .catch(error => console.error(error))
+  // }, []);
 
   const handlePredicted = (newState) => {
     setPredicted(newState);
   }
 
-  const uploadStatus = (newState) => {
-    setPhotoUploaded(newState)
+  const uploadStatus = (uploadStatus, img) => {
+    setPhotoUploaded(uploadStatus)
+    setPhoto(img);
+    console.log("img in app: "+img.name)
   }
 
 
@@ -29,11 +44,12 @@ function App() {
           {photoUploaded && 
             <SkinCancerClassifier 
               predictionStatus={handlePredicted}
+              curImg = {photo}
             />
           }
         </div>
       </div>
-
+          {/* <p>The current time is {time}</p> */}
     </div>
   );
 }
